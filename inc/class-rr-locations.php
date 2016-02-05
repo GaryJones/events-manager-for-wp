2ffe575207e1b15a90c93effac2f27e788a9cd86
Ryan Hellyer <ryanhellyer@gmail.com>
2016-02-05 12:00:32 +0100

    Implementing backend for event locations

--- inc/class-rr-locations.php
+++ inc/class-rr-locations.php
@@ -41,14 +41,69 @@ class RR_Locations {
 	 */
 	public function meta_box() {
 
-		?>
+		$location = get_post_meta( get_the_ID(), '_location', true );
+		if ( isset( $location['latitude'] ) ) {
+			$latitude = $location['latitude'];
+		} else {
+			$latitude = '52.511738';
+		}
+		if ( isset( $location['longitude'] ) ) {
+			$longitude = $location['longitude'];
+		} else {
+			$longitude = '13.466479';
+		}
 
-		<p>
-			<label for="_location"><strong><?php _e( 'Location', 'rrewc' ); ?></strong></label>
-			<br />
-			<input type="text" name="location" id="_location" value="<?php echo esc_attr( get_post_meta( get_the_ID(), '_location', true ) ); ?>" />
-			<input type="hidden" id="location-nonce" name="location-nonce" value="<?php echo esc_attr( wp_create_nonce( __FILE__ ) ); ?>">
-		</p><?php
+		$embed_url = 'https://maps.google.com/maps?q=' . $latitude . ',' . $longitude . '&z=14&output=embed&iwloc=0';
+
+		?>
+		<style>
+		.rr-location {
+			display: inline-block;
+		}
+		table.rr-location {
+			width: 39%;
+		}
+		iframe.rr-location {
+			width: 59%;
+			height: 300px;
+		}
+		</style>
+
+		<table class="rr-location">
+			<tr>
+				<td><label for="latitude"><strong><?php _e( 'Latitude', 'rrewc' ); ?></strong></label></td>
+				<td>
+					<input type="text" name="location[latitude]" id="latitude" value="<?php echo esc_attr( $latitude ); ?>" />
+				</td>
+			</tr>
+			<tr>
+				<td><label for="longitude"><strong><?php _e( 'Longitude', 'rrewc' ); ?></strong></label></td>
+				<td>
+					<input type="text" name="location[longitude]" id="longitude" value="<?php echo esc_attr( $longitude ); ?>" />
+				</td>
+			</tr>
+		</table>
+
+		<iframe id="rr-map" class="rr-location" src="<?php echo esc_url( $embed_url ); ?>" frameborder="0" allowfullscreen></iframe>
+
+		<script>
+
+			var latitude = document.getElementById("latitude");
+			latitude.addEventListener("change", set_map_location);
+
+			var longitude = document.getElementById("longitude");
+			longitude.addEventListener("change", set_map_location);
+
+			function set_map_location() {
+				var embed_url = 'https://maps.google.com/maps?q='+latitude.value+','+longitude.value+'&z=14&output=embed&iwloc=0';
+				var map = document.getElementById("rr-map");
+				map.src = embed_url;
+			}
+			set_map_location();
+
+		</script>
+
+		<input type="hidden" id="location-nonce" name="location-nonce" value="<?php echo esc_attr( wp_create_nonce( __FILE__ ) ); ?>"><?php
 	}
 
 	/**
