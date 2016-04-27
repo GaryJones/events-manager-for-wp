fc50687c1444c8c3dfd468e11267e4bd25e4aa0a
Ryan Hellyer <ryanhellyer@gmail.com>
2016-04-27 16:25:26 +0200

    Moving post over to new blog #3

--- inc/class-em4wp-post-mover.php
+++ inc/class-em4wp-post-mover.php
@@ -15,11 +15,6 @@ class EM4WP_Post_Mover {
 		$this->sites = $sites;
 
 		add_action( 'save_post', array( $this, 'save_post' ), 200 );
-return;
-
-		echo $this->post_type."\n\n";
-		print_r( $sites );die;
-
 	}
 
 	/**
@@ -46,7 +41,7 @@ return;
 				if ( get_current_blog_id() == $from ) {
 
 					// Copy $post_id between relevant sites within $this->sites.
-					$this->duplicate_over_multisite( $from, $to, $this->post_type, 'author??', 'prefix???' );
+					$this->duplicate_over_multisite( $from, $to, $post_id, $this->post_type, 'author??' );
 					echo 'Saving Event!';
 					die;
 
@@ -65,10 +60,10 @@ return;
 	 * This is the main core function on Multisite Post Duplicator that processes the duplication of a post on a network from one
 	 * site to another
 	 * 
-	 * @param int $from The ID of the source post to copy
+	 * @param int $from The ID of the source blog to copy from.
 	 * @param int $to The ID of the destination blog to copy to.
+	 * @param int $post_id The ID of the source post to copy.
 	 * @param string $post_type The destination post type.
-	 * @param string $prefix Optional prefix to be used on the destination post.
 	 * 
 	 * @return array An array containing information about the newly created post
 	 * 
@@ -79,68 +74,55 @@ return;
 	 *          site_name    => 'Another Site'
 	 * 
 	 */
-	public function duplicate_over_multisite( $from, $to, $post_type, $prefix ) {
+	public function duplicate_over_multisite( $from, $to, $post_id, $post_type ) {
 
 		$post_author = 1; // Should pick up origin post author
 		$post_status = 'publish'; // Should pick up origin post status
 
-		//Collect function arguments into a single variable
+		// Collect function arguments into a single variable
 		$mpd_process_info = array(
-			'source_id'             => $from,
-			'destination_id'        => $to,
 			'post_type'             => $post_type,
 			'post_author'           => $post_author,
-			'prefix'                => $prefix,
 			'requested_post_status' => $post_status
 		);
 
-		//Get plugin options
-		$options    = get_option( 'mdp_settings' );
-		//Get the object of the post we are copying
-		$mdp_post   = get_post($mpd_process_info['source_id']);
-		//Get the title of the post we are copying
-		$title      = get_the_title($mdp_post);
-		//Get the tags from the post we are copying
-		$sourcetags = wp_get_post_tags( $mpd_process_info['source_id'], array( 'fields' => 'names' ) );
-		//Get the ID of the sourse blog
+		// Get the object of the post we are copying
+		$post   = get_post( $post_id );
+		// Get the title of the post we are copying
+		$title      = get_the_title( $post );
+		// Get the tags from the post we are copying
+		$sourcetags = wp_get_post_tags( $post_id, array( 'fields' => 'names' ) );
+		// Get the ID of the sourse blog
 		$source_blog_id  = get_current_blog_id();
-		//Get the categories for the post
-//		$source_categories = mpd_get_objects_of_post_categories($mpd_process_info['source_id'], $mpd_process_info['post_type']);
+		// Get the categories for the post
+//		$source_categories = mpd_get_objects_of_post_categories( $post_id, $mpd_process_info['post_type']);
 
-		//Format the prefix into the correct format if the user adds their own whitespace
-		if($mpd_process_info['prefix'] != ''){
-
-			$mpd_process_info['prefix'] = trim($mpd_process_info['prefix']) . ' ';
-
-		}
-
-		//Using the orgininal post object we now want to insert our any new data based on user settings for use
-		//in the post object that we will be adding to the destination site
-		$mdp_post = array(
-
-				'post_title'    => $mpd_process_info['prefix'] . $title,
-				'post_status'   => $mpd_process_info['requested_post_status'],
-				'post_type'     => $mpd_process_info['post_type'],
-				'post_author'   => $mpd_process_info['post_author'],
-				'post_content'  => $mdp_post->post_content,
-				'post_excerpt'  => $mdp_post->post_excerpt,
-				'post_content_filtered' => $mdp_post->post_content_filtered
-
+		// Using the orgininal post object we now want to insert our any new data based on user settings for use
+		// in the post object that we will be adding to the destination site
+		$post = array(
+			'post_title'    => $title,
+			'post_status'   => $mpd_process_info['requested_post_status'],
+			'post_type'     => $mpd_process_info['post_type'],
+			'post_author'   => $mpd_process_info['post_author'],
+			'post_content'  => $post->post_content,
+			'post_content'  => $post->post_content,
+			'post_excerpt'  => $post->post_excerpt,
+			'post_content_filtered' => $post->post_content_filtered
 		);
 
-		$data              = get_post_custom( $mdp_post );
-		$meta_values       = get_post_meta( $mpd_process_info['source_id'] );
-//		$featured_image    = mpd_get_featured_image_from_source($mpd_process_info['source_id']);
+		$data              = get_post_custom( $post );
+		$meta_values       = get_post_meta( $post_id );
+//		$featured_image    = mpd_get_featured_image_from_source( $post_id );
 
-		//If we are copying the sourse post to another site on the network we will collect data about those 
-		//images.
-		if($mpd_process_info['destination_id'] != $source_blog_id){
+		// If we are copying the sourse post to another site on the network we will collect data about those 
+		// images.
+		if( $to != $source_blog_id){
 
-//			$attached_images = mpd_get_images_from_the_content($mpd_process_info['source_id']);
+//			$attached_images = mpd_get_images_from_the_content( $post_id );
 
 			if($attached_images){
 
-				$attached_images_alt_tags   = mpd_get_image_alt_tags($attached_images);
+//				$attached_images_alt_tags   = mpd_get_image_alt_tags($attached_images);
 				
 			}
 
@@ -151,13 +133,14 @@ return;
 		}
 
 		////////////////////////////////////////////////
-		//Tell WordPress to work in the destination site
-		switch_to_blog($mpd_process_info['destination_id']);
+		// Tell WordPress to work in the destination site
+		switch_to_blog( $to );
 		////////////////////////////////////////////////
 
-		//Make the new post
-		$post_id = wp_insert_post($mdp_post);
-		//Add the source post meta to the destination post
+		// Make the new post
+		$post_id = wp_insert_post( $post );
+
+		// Add the source post meta to the destination post
 		foreach ( $data as $key => $values) {
 
 		   foreach ($values as $value) {
@@ -168,7 +151,7 @@ return;
 
 		}
 		
-		//Copy the meta data collected from the sourse post to the new post
+		// Copy the meta data collected from the sourse post to the new post
 		foreach ($meta_values as $key => $values) {
 
 		   foreach ($values as $value) {
@@ -187,19 +170,15 @@ return;
 			}
 
 		}
-		//If there were media attached to the sourse post content then copy that over
+
+		// If there were media attached to the sourse post content then copy that over
 		if($attached_images){
-			//Check that the users plugin settings actually want this process to happen
-			if(isset($options['mdp_copy_content_images']) || !$options ){
-				
-				mpd_process_post_media_attachements($post_id, $attached_images, $attached_images_alt_tags, $source_blog_id, $to);
-
-			}
-
+			mpd_process_post_media_attachements($post_id, $attached_images, $attached_images_alt_tags, $source_blog_id, $to);
 		}
-		//If there was a featured image in the sourse post then copy it over
+
+		// If there was a featured image in the sourse post then copy it over
 		if($featured_image){
-			//Check that the users plugin settings actually want this process to happen
+			// Check that the users plugin settings actually want this process to happen
 			if(isset($options['mdp_default_featured_image']) || !$options ){
 
 				mpd_set_featured_image_to_destination( $post_id, $featured_image ); 
@@ -207,9 +186,9 @@ return;
 			}
 
 		}
-		//If there were tags in the sourse post then copy them over
+		// If there were tags in the sourse post then copy them over
 		if($sourcetags){
-			//Check that the users plugin settings actually want this process to happen
+			// Check that the users plugin settings actually want this process to happen
 			if(isset($options['mdp_default_tags_copy']) || !$options ){
 
 				wp_set_post_tags( $post_id, $sourcetags );
@@ -218,7 +197,7 @@ return;
 			
 		}
 
-		//If there were categories in the sourse post then copy them over
+		// If there were categories in the sourse post then copy them over
 		if($source_categories){
 
 			if(isset($options['mdp_copy_post_categories']) || !$options ){
@@ -229,13 +208,13 @@ return;
 
 		}
 		
-		//Collect information about the new post 
+		// Collect information about the new post 
 		$site_edit_url = get_edit_post_link( $post_id );
-		$blog_details  = get_blog_details($mpd_process_info['destination_id']);
+		$blog_details  = get_blog_details( $to );
 		$site_name     = $blog_details->blogname;
 
 		//////////////////////////////////////
-		//Go back to the current blog so we can update information about the action that just took place
+		// Go back to the current blog so we can update information about the action that just took place
 		restore_current_blog();
 		//////////////////////////////////////
 
