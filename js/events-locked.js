e0a2bb7b685d88a6ea64f255ea682a82476a4ad6
Ryan Hellyer <ryanhellyer@gmail.com>
2016-04-29 11:55:57 +0200

    Removing unused JS from core plugin #3

--- js/events-locked.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){ 
-	var publish_button = document.getElementById( 'publish' );
-	publish_button.value = em4wp_publish_text;
-	publish_button.disabled = true;
-
-	var trash_button = document.getElementById( 'delete-action' );
-	trash_button.innerHTML = '<a href=\"'+em4wp_source_post+'\" class=\"submitdelete deletion\">'+em4wp_locked_text+'</span>';
-	//.style.display = 'none';
-}, false);
