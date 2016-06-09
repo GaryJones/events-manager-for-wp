670aa2bf4b99cb64698ef4455f65e7d40f88c511
Ryan Hellyer <ryanhellyer@gmail.com>
2016-06-09 15:25:11 +0200

    Outputting event type to front end #5

--- inc/class-em4wp-event-type.php
+++ inc/class-em4wp-event-type.php
@@ -5,6 +5,8 @@
  */
 class EM4WP_Event_Type extends EM4WP_Events_Core {
 
+	public $taxonomy_slug = 'event-type';
+
 	/**
 	 * Class constructor.
 	 */
@@ -12,7 +14,8 @@ class EM4WP_Event_Type extends EM4WP_Events_Core {
 
 		parent::__construct();
 
-		add_action( 'init',    array( $this, 'register_taxonomy' ) );
+		add_action( 'init',           array( $this, 'register_taxonomy' ) );
+		add_filter( 'the_content',    array( $this, 'the_content' ), 27 );
 	}
 
 	/**
@@ -20,14 +23,54 @@ class EM4WP_Event_Type extends EM4WP_Events_Core {
 	 */
 	public function register_taxonomy() {
 		register_taxonomy(
-			'event-type',
+			$this->taxonomy_slug,
 			'event',
 			array(
 				'label'        => __( 'Event Type' ),
-				'rewrite'      => array( 'slug' => 'event-type' ),
+				'rewrite'      => array( 'slug' => $taxonomy_slug ),
 				'hierarchical' => false,
 			)
 		);
+	}
+
+	/**
+	 * the_content() filter.
+	 *
+	 * @param  string  $content  The post content
+	 * @return string  The modified post content
+	 */
+	public function the_content( $content ) {
+
+		if ( 'event' != get_post_type() ) {
+			return;
+		}
+
+		$terms = get_terms( array(
+			'taxonomy'   => $this->taxonomy_slug,
+			'hide_empty' => false,
+		) );
+
+		if ( $terms ) {
+			$content .= '
+			<div class="em4wp-one-half">
+				<h3>' . __( 'Event type', 'events-manager-for-wp' ) . '</h3>
+				<ul>';
+
+			foreach ( $terms as $term ) {
+				$url = get_term_link( $term->term_id );
+				echo '
+				<li>
+					<a href="' . esc_url( $url ) . '">
+						' . esc_html( $term->name ) . '
+					</a>
+				</li>';
+			}
+
+			$content .= '</ul>
+			</div>';
+		}
+
+		return $content;
 	}
 
 }
